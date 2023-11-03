10a3a7eaa7e9dc6c1955a222c178b348f42161d1
asishab <asish.bishoi@gmail.com>
2023-11-03 11:10:33 +0530

    $Code committed_0

--- cmd-from-python.py
+++ cmd-from-python.py
@@ -1,7 +1,19 @@
 import os
 import time
 
-commit_message = "Code committed from Python Script"
+no_of_times_the_commit_runs = 0
+max_no_of_times_the_commit_runs = 1000
+delay_timer_to_wait_for_adding_files_to_stage = 2
+
+# for i in range(no_of_times_the_commit_runs, max_no_of_times_the_commit_runs):
+#     commit_message = f"Code committed_{no_of_times_the_commit_runs}" # It will change the current file, so that it can be added to the Git Stage
+#     os.system("git add .")
+#     time.sleep(delay_timer_to_wait_for_adding_files_to_stage)
+#     os.system(f'git commit -m "${commit_message}"')
+
+commit_message = f"Code committed_{no_of_times_the_commit_runs}" # It will change the current file, so that it can be added to the Git Stage
 os.system("git add .")
-time.sleep(4)
+time.sleep(delay_timer_to_wait_for_adding_files_to_stage)
 os.system(f'git commit -m "${commit_message}"')
+
+
