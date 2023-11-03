019ccdff221d579e60e8d7a61ea292bc7eab0c19
asishab <asish.bishoi@gmail.com>
2023-11-03 11:12:22 +0530

    $Code committed_0

--- cmd-from-python.py
+++ cmd-from-python.py
@@ -2,18 +2,15 @@ import os
 import time
 
 no_of_times_the_commit_runs = 0
-max_no_of_times_the_commit_runs = 1000
+max_no_of_times_the_commit_runs = 1
 delay_timer_to_wait_for_adding_files_to_stage = 2
 
-# for i in range(no_of_times_the_commit_runs, max_no_of_times_the_commit_runs):
-#     commit_message = f"Code committed_{no_of_times_the_commit_runs}" # It will change the current file, so that it can be added to the Git Stage
-#     os.system("git add .")
-#     time.sleep(delay_timer_to_wait_for_adding_files_to_stage)
-#     os.system(f'git commit -m "${commit_message}"')
+for i in range(no_of_times_the_commit_runs, max_no_of_times_the_commit_runs):
+    commit_message = f"Code committed_{no_of_times_the_commit_runs}" # It will change the current file, so that it can be added to the Git Stage
+    os.system("git add .")
+    time.sleep(delay_timer_to_wait_for_adding_files_to_stage)
+    os.system(f'git commit -m "${commit_message}"')
 
-commit_message = f"Code committed_{no_of_times_the_commit_runs}" # It will change the current file, so that it can be added to the Git Stage
-os.system("git add .")
-time.sleep(delay_timer_to_wait_for_adding_files_to_stage)
-os.system(f'git commit -m "${commit_message}"')
+
 
 
