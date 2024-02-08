6bdb437da9f0ece7a2545b429ac5e21e560bf117
Asish <58765530+AsishAB@users.noreply.github.com>
2024-02-08 20:16:56 +0530

    Read Description

--- S3 to Dynamodb Python.py
+++ S3 to Dynamodb Python.py
@@ -9,7 +9,7 @@ import boto3
 bucket_name = 'aws-bmo-podr'
 prefix = 'trigger-for-lambda-dynamodb'
 object_key = f'{prefix}/Main File.txt'
-response = []
+
 
 s3_client = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
@@ -29,42 +29,45 @@ def write_to_dynamodb(item):
     
 def read_from_dynamodb_by_partitionkey(item):
     try:
+        
         data = table.get_item(
                 Key = item
         )
         
         if 'Item' in data:
             return {
-                "success": 1
+                "success": 1,
                 "data" : data['Item']
 
             }
         else:
             return {
-                "success": 0
+                "success": 0,
                 "data" : "No data found"
 
             }       
     except Exception as e:
         return {
-                "success": 2
+                "success": 2,
                 "data" : e
 
             }
-def update_to_dynamodb(item):
+def update_to_dynamodb(item, update_expression, expression_attribute_values):
     try:
-        response = table.update_item(
-            Key=key,
+        update_response = table.update_item(
+            Key=item,
             UpdateExpression=update_expression,
             ExpressionAttributeValues=expression_attribute_values,
             ReturnValues='UPDATED_NEW'
         )
 
-        return f"Update Item succeeded: {response}"
+        return f"Update Item succeeded: {update_response}"
     except Exception as e:
         return e
    
 def lambda_handler(event, context):
+    response = []
+    
     # bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     # object_key = event["Records"][0]["s3"]["object"]["key"]
     
@@ -78,19 +81,33 @@ def lambda_handler(event, context):
         file_content = object_content['Body'].read().decode('utf-8')
         # print(f"File Content in {fl} file = {file_content} ")
         data = json.loads(file_content)
+       
         read_key = {
-            'tbl_key': data.tbl_name
+            'tbl_key': f"{data['tbl_key']}"
         }
         read_response = read_from_dynamodb_by_partitionkey(read_key)
-        print(read_response)
-        # resp = write_to_dynamodb(data_to_write)
+        if read_response['success'] == 0:
+            print(f"Success = {read_response['success']}, meaning READ")
+            resp = write_to_dynamodb(data)
+            response.append(resp)
+        elif read_response['success'] == 1:
+            print(f"Success = {read_response['success']}, meaning WRITE")
+            update_expression = 'SET tbl_name = :new_value, table_id = :new_value2'
+            expression_attribute_values = {
+                 ':new_value': 'Helofknwlejnre_21321y2349',
+                 ':new_value2': '12312423432'
+            }
+            resp = update_to_dynamodb(read_key, update_expression, expression_attribute_values)
+            response.append(resp)
+        else:
+            resp = read_response['data']
+            response.append(resp)
+            
 
         # Specify the update expression and attribute values
-        update_expression = 'SET attribute_name = :new_value'
-        expression_attribute_values = {
-            ':new_value': 'UpdatedValue'
-        }
-        response.append(resp)
+        # 
+        # 
+        # response.append(resp)
         
     
     # data_to_write = json.loads(body)
@@ -98,7 +115,7 @@ def lambda_handler(event, context):
     # response = read_from_dynamodb_by_partitionkey(item1)
     
     # response = write_to_dynamodb(data_to_write)
-    # response = body
+    
     print(response)
     return response
         
